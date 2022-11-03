d85335556c76af5587613dc4d67612ed026c3910
ParkYoungWoong <thesecon@gmail.com>
2022-11-03 09:40:54 +0900

    테스트 업데이트

--- js/share.js
+++ js/share.js
@@ -1,13 +1,14 @@
-const shareBtn = document.querySelector('.btn-share')
+const shareBtn = document.querySelector('.share')
 
 shareBtn.addEventListener('click', () => {
-  if (navigator.share) {
-    navigator.share({
-      title: '내 안에 숨어있는 직업캐 찾기!',
-      text: '누구나 찰떡인 직업이 있어요! 내 안에 숨어있는 직업캐를 찾아보세요!',
-      url: location.href,
-    })
-  } else {
-    navigator.clipboard.writeText(location.href)
-  }
+  navigator.clipboard.writeText(location.href)
+  // if (navigator.share) {
+  //   navigator.share({
+  //     title: '내 안에 숨어있는 직업캐 찾기!',
+  //     text: '누구나 찰떡인 직업이 있어요! 내 안에 숨어있는 직업캐를 찾아보세요!',
+  //     url: location.href,
+  //   })
+  // } else {
+  //   navigator.clipboard.writeText(location.href)
+  // }
 })
